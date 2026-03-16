1cc0b0df26537be57181db326890231135d63c8f
MrAaz <96545400+MrAaz0@users.noreply.github.com>
2026-03-16 15:58:06 +0300

    Create PVZGame.java as the main entry point

---
+++ src/com/pvz/PVZGame.java
@@ -0,0 +1,6 @@
+public class PVZGame {
+    public static void main(String[] args) {
+        // Main entry point of the Plants vs Zombies game
+        System.out.println("Starting Plants vs Zombies...");
+    }
+}
